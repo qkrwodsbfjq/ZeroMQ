43fb48312542a02a798c2b56e40ef3407a45cee2
Jaeyun Park <qkrwodbsfjq@khu.ac.kr>
2023-12-03 00:33:59 +0900

    Feat: ZeroMQ Client 구현

---
+++ ZeroMQ-client/src/main/java/org/example/ClientController.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.client.*;
+
+import java.util.Scanner;
+
+public class ClientController {
+    public static void main(String[] args) throws Exception {
+        int num = -1;
+        Scanner sc = new Scanner(System.in);
+        while (num != 0) {
+            System.out.println("========================");
+            System.out.println(" 0 : Quit");
+            System.out.println(" 1 : ReqRepBasicClient");
+            System.out.println(" 2 : PubSubBasicClient");
+            System.out.println(" 3 : PubSubAndPullPushClient");
+            System.out.println(" 4 : PubSubAndPullPushClientV2");
+            System.out.println(" 5 : DealerRouterAsyncClient");
+            System.out.println("========================");
+
+            num = sc.nextInt();
+            switch (num) {
+                case 0:
+                    break;
+                case 1:
+                    ReqRepBasicClient.start();
+                    break;
+                case 2:
+                    PubSubBasicClient.start(new String[]{});
+                    break;
+                case 3:
+                    PubSubAndPullPushClient.start();
+                    break;
+                case 4:
+                    PubSubAndPullPushClientV2.start(new String[]{"client#1"});
+                    break;
+                case 5:
+                    DealerRouterAsyncClient.start(new String[]{"client#1"});
+                    break;
+                case 6:
+                    DealerRouterAsyncClientThread.start(new String[]{"client#1"});
+                default:
+                    System.out.println("1~5 중에 선택해주세요.\n");
+            }
+        }
+    }
+}
---
+++ ZeroMQ-client/src/main/java/org/example/client/PubSubBasicClient.java
@@ -0,0 +1,50 @@
+package org.example.client;
+
+import java.util.StringTokenizer;
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+import static java.lang.Thread.sleep;
+
+public class PubSubBasicClient {
+    public static void start(String args[]) {
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
+
+            System.out.println("Collecting updates from weather server");
+            subscriber.connect("tcp://localhost:5556");
+
+            String filter = (args.length > 0) ? args[0] : "10001";
+            subscriber.subscribe(filter.getBytes(ZMQ.CHARSET));
+
+            long total_temp = 0;
+            int update_nbr;
+            for (update_nbr = 0; update_nbr < 20; update_nbr++) {
+                String string = subscriber.recvStr(0).trim();
+
+                StringTokenizer sscanf = new StringTokenizer(string, " ");
+                int zipcode = Integer.valueOf(sscanf.nextToken());
+                int temperature = Integer.valueOf(sscanf.nextToken());
+                int relhumidity = Integer.valueOf(sscanf.nextToken());
+
+                System.out.println(
+                        "Receive temperature for zipcode '" + zipcode
+                                + "' was " + temperature + " F"
+                );
+                total_temp += temperature;
+                sleep(1000);
+            }
+
+            System.out.println(
+                    String.format(
+                            "Average temperature for zipcode '%s' was %.2f F.",
+                            filter,
+                            (double) total_temp / (update_nbr + 1)
+                    )
+            );
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
